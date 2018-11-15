d6a3f3968eeaad8214aa11dca24f8f2d8dcc41e9
Wendy Billings <wbillings@studentbody.byu.edu>
2018-11-14 23:05:55 -0700

    super basic framework

---
+++ script.js
@@ -0,0 +1,20 @@
+function getRandom() {
+
+    //make actual call to get the array full of randomly selected cards from the database
+        //store as urls or relative paths??
+
+    //loop through and assign
+    //but for now, filler, do this to show it works:
+    $("#1").attr("src", "https://goo.gl/fQx5gw");
+    $("#2").attr("src", "https://goo.gl/fQx5gw");
+    $("#3").attr("src", "https://goo.gl/fQx5gw");
+    $("#4").attr("src", "https://goo.gl/fQx5gw");
+    $("#5").attr("src", "https://goo.gl/fQx5gw");
+
+    $("#6").attr("src", "https://goo.gl/fQx5gw");
+    $("#7").attr("src", "https://goo.gl/fQx5gw");
+    $("#8").attr("src", "https://goo.gl/fQx5gw");
+    $("#9").attr("src", "https://goo.gl/fQx5gw");
+    $("#10").attr("src", "https://goo.gl/fQx5gw");
+
+}
